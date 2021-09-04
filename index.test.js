924b1b0809e6b387c84f3cb0d6fdf7264a2ccbee
Selim <selimabdelmonsef@gmail.com>
2021-09-04 18:31:46 +0200

    test(index.test.js): find function unit test

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ import * as main from './index.js'
 import * as naive from './index.naive.js'
 
 describe('List', () => {
-  fit('should create list', () => {
+  it('should create list', () => {
     const list = main.List({ initial: [1, 2, 3, 4], initialOrder: true });
     const expected = [1, 2, 3, 4];
     const actual = list.items;
@@ -10,7 +10,7 @@ describe('List', () => {
     expect(actual).toEqual(expected);
   })
 
-  fit('should sort initial list items index.js', () => {
+  it('should sort initial list items index.js', () => {
     const list = main.List({ initial: [4, 3, -1, 1, 2, 5, 0] });
     const expected = [-1, 0, 1, 2, 3, 4, 5];
     const actual = list.items;
@@ -18,7 +18,7 @@ describe('List', () => {
     expect(actual).toEqual(expected);
   });
 
-  fit('should sort initial list items in index.naive.js', () => {
+  it('should sort initial list items in index.naive.js', () => {
     const list = naive.List({ initial: [100, -100, 10, 9, 200, -300] });
     const expected = [-300, -100, 9, 10, 100, 200];
     const actual = list.items;
@@ -42,13 +42,21 @@ describe('List', () => {
     expect(actual).toEqual(expected)
   })
 
-  it('should findIndex of b at list a', () => {
-    const list = List({ initial: [1, 2, 3, 4], initialOrder: true  })
+  it('should findIndex of b at list a in index.js', () => {
+    const list = main.List({ sortKey: 'value', initial: [{value: 1}, {value: 2}, {value: 3}, {value: 4}], initialOrder: true  });
     const expected = 2;
-    const actual = list.findIndex(3)
+    const actual = list.findIndex(3);
+    expect(actual).toEqual(expected);
+  });
 
-    expect(actual).toEqual(expected)
-  })
+  it('should findIndex of b at list a in index.naive.js', () => {
+    const list = naive.List({ sortKey: 'value', initial: [1, 2, 3, 4], initialOrder: true  });
 
-  // PLEASE cover the rest of the methods/scenarios with tests
+    const expected = 2;
+    const actual = list.findIndex(3);
+
+    expect(actual).toEqual(expected);
+  });
+
+
 })
