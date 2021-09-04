ff751894bf0279b99ceae9ed046de59a9dba5760
Selim <selimabdelmonsef@gmail.com>
2021-09-04 13:05:40 +0200

    test(index.test.js): sorting unit test

--- index.test.js
+++ index.test.js
@@ -1,28 +1,29 @@
-import { List } from './index.js'
+import * as main from './index.js'
+import * as naive from './index.naive.js'
 
 describe('List', () => {
-  it('should create list', () => {
-    const list = List({ initial: [1, 2, 3, 4], initialOrder: true })
+  fit('should create list', () => {
+    const list = main.List({ initial: [1, 2, 3, 4], initialOrder: true });
     const expected = [1, 2, 3, 4];
-    const actual = list.items
+    const actual = list.items;
 
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual(expected);
   })
 
-  it('should sort initial list items', () => {
-    const list = List({ initial: [4, 3, -1, 1, 2, 5, 0] })
+  fit('should sort initial list items index.js', () => {
+    const list = main.List({ initial: [4, 3, -1, 1, 2, 5, 0] });
     const expected = [-1, 0, 1, 2, 3, 4, 5];
-    const actual = list.items
+    const actual = list.items;
 
-    expect(actual).toEqual(expected)
-  })
+    expect(actual).toEqual(expected);
+  });
 
-  it('should sort initial list items', () => {
-    const list = List({ initial: [100, -100, 10, 9, 200, -300] })
+  fit('should sort initial list items in index.naive.js', () => {
+    const list = naive.List({ initial: [100, -100, 10, 9, 200, -300] });
     const expected = [-300, -100, 9, 10, 100, 200];
-    const actual = list.items
+    const actual = list.items;
 
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual(expected);
   })
 
   it('should insert b into list a', () => {
