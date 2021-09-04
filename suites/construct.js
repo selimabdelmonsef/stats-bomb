3d7e136afc8dc8cb2425ed6939f874a07de763ca
Selim <selimabdelmonsef@gmail.com>
2021-09-04 13:04:50 +0200

    feat(index.js/ construct.js/ benchmark.js): sorting function

--- suites/construct.js
+++ suites/construct.js
@@ -5,22 +5,29 @@ import { getRandomItems } from '../utils.js'
 import * as main from '../index.js'
 import * as naive from '../index.naive.js'
 
-const itemsCount = 9999
-const randomItems = getRandomItems(itemsCount)
-const sortedItems = times(i => ({ value: i }), itemsCount)
+const itemsCount = 9999;
+const randomItems = getRandomItems(itemsCount);
+const sortedItems = times(i => ({ value: i }), itemsCount);
 
 // Construct SORTED
 b.suite(
   'Construct with an already sorted list of items',
-
   b.add('naive', () => {
-    naive.List({ initial: sortedItems, sortKey: 'value', initialOrder: true })
+    naive.List({
+      sortKey: 'value',
+      initial: sortedItems,
+      initialOrder: true
+    })
   }),
 
   b.add('main', () => {
-    main.List({ initial: sortedItems, sortKey: 'value', initialOrder: true })
+    main.List(
+      {
+        sortKey: 'value',
+        initial: sortedItems,
+        initialOrder: true
+      })
   }),
-
   b.cycle(),
   b.complete(),
 )
@@ -29,15 +36,22 @@ b.suite(
 // Construct UNSORTED
 b.suite(
   'Construct unsorted list of items',
-
+  
   b.add('naive', () => {
-    naive.List({ initial: randomItems, sortKey: 'value' })
+    naive.List({
+      sortKey: 'value',
+      initial: randomItems,
+      initialOrder: false
+    });
   }),
-
   b.add('main', () => {
-    main.List({ initial: randomItems, sortKey: 'value' })
+    main.List({
+      sortKey: 'value',
+      initial: randomItems,
+      initialOrder: false
+      
+    });
   }),
-
   b.cycle(),
   b.complete(),
 )
